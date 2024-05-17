d1024031b6dbd6a21e90929580ff81593f8a5b11
Suraj Biswas <surajbiswas182003@gmail.com>
2024-05-17 14:56:56 +0530

    little mod

--- lua/plugins/lsp/mason.lua
+++ lua/plugins/lsp/mason.lua
@@ -5,10 +5,15 @@ return {
     "WhoIsSethDaniel/mason-tool-installer.nvim",
   },
   config = function()
+    -- import mason
     local mason = require("mason")
 
+    -- import mason-lspconfig
     local mason_lspconfig = require("mason-lspconfig")
 
+    local mason_tool_installer = require("mason-tool-installer")
+
+    -- enable mason and configure icons
     mason.setup({
       ui = {
         icons = {
@@ -17,14 +22,18 @@ return {
           package_uninstalled = "✗",
         },
       },
-    }) 
+    })
+
     mason_lspconfig.setup({
       -- list of servers for mason to install
       ensure_installed = {
         "tsserver",
         "html",
         "cssls",
+        "tailwindcss",
+        "svelte",
         "lua_ls",
+        "graphql",
         "emmet_ls",
         "prismals",
         "pyright",
@@ -35,9 +44,11 @@ return {
       ensure_installed = {
         "prettier", -- prettier formatter
         "stylua", -- lua formatter
+        "isort", -- python formatter
+        "black", -- python formatter
         "pylint",
         "eslint_d",
       },
     })
-  end
+  end,
 }
